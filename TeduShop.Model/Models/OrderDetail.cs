06b391f031ee01fb1a1620444560b572bea550f4
linda <phucle2901@gmail.com>
2023-03-13 17:21:53 +0700

    add class

---
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace TeduShop.Model.Models
+{
+    public class OrderDetail
+    {
+        [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        [Column(Order =1)]
+        public int OrderID { set; get; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int ProductID { set; get; }
+
+        public int Quantity { set; get; }
+
+        public decimal Price { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+    }
+    }
+}
